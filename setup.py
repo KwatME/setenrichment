91c760e81d5b3b0f4ca2b612d45ff390b9e4c3b3
KwatME <kwat.me@icloud.com>
2021-07-21 16:43:08 -0600

    16:42

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name=na,
     version="0.1.0",
     url="https://github.com/KwatME/gsea",
-    python_requires=">=3.6",
+    python_requires=">=3.6.0",
     install_requires=["julia", "pandas"],
     packages=[na],
 )
